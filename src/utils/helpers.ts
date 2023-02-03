6ca09026d2f3075eae8d0e8dbdbba723290a63b1
Angel Caceres <caceres840@gmail.com>
2023-02-03 13:20:11 -0600

    Files

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -3,17 +3,17 @@ import multer from 'multer';
 import AWS from 'aws-sdk';
 import multers3 from 'multer-s3';
 
-const spacesEndpoint = new AWS.Endpoint('nyc3.digitaloceanspaces.com');
+const spacesEndpoint = new AWS.Endpoint(String(process.env.SP_ENDPOINT));
 const s3: any = new AWS.S3({
   endpoint: spacesEndpoint,
-  accessKeyId: 'DO00EW7UXVPY4RML34YG',
-  secretAccessKey: 'XsJCmB5oofFJj0W4nSz/xDTxC1Z2E7VtNjw+Ts4m6Ww',
+  accessKeyId: process.env.ACCESS_KEY,
+  secretAccessKey: process.env.SECRET_KEY,
 });
 
 const upload = multer({
   storage: multers3({
     s3,
-    bucket: 'susasv',
+    bucket: String(process.env.BUCKET),
     contentType: multers3.AUTO_CONTENT_TYPE,
     acl: 'public-read',
     key: function (request, file, cb) {
